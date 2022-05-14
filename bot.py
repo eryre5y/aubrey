a041eb241fb8027a0fed441592eb7b6bfd4f0cd0
eryre5y <mikus1337112@gmail.com>
2022-05-14 18:04:24 +0300

    birth (watching tutorials to understand this shit)

--- bot.py
+++ bot.py
@@ -1,1 +1,12 @@
 #!/usr/bin/env python3
+import dstk
+import discord
+from discord.ext import commands
+
+client = commands.Bot(command_prefix = '>')
+
+@client.event
+async def on_ready():
+    print('aubrey is ready to kill everyone!')
+    
+client.run(dstk.token)
