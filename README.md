19dd670dc0edaea56548497e60199103c24a79e6
eryre5y <mikus1337112@gmail.com>
2022-05-15 16:35:32 +0300

    basic commands, modify read.me, add requirements.txt

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# aubrey
-чак чак стал обри с функцией discord.py
+# "aubrey" - discord bot
+another discord bot but it uses discord.py instead of hikari
+
+* https://github.com/Rapptz/discord.py
+
+# installation
+```
+pip install -r requirements.txt
+```
+
+# usage
+add `dstk.py` then insert in this file your discord bot token
+```
+token='_ur bot token_'
+```
--- bot.py
+++ bot.py
@@ -1,9 +1,40 @@
 #!/usr/bin/env python3
 import dstk
 import discord
+import random
 from discord.ext import commands
 
 client = commands.Bot(command_prefix = '>')
+
+@client.command()
+async def ping(ctx):
+    await ctx.send(f'pong ({round(client.latency * 1000)}ms)')
+    
+@client.command()
+async def text(ctx, *, text):
+    await ctx.send(text)
+    
+@client.command(aliases=['8ball', 'eightball'])
+async def _8ball(ctx, *, question):
+    responses = ['yes.',
+                 'no.',
+                 'ask urself.',
+                 'i don\'t care.',
+                 'who knows.',
+                 'why do you ask me this?'
+                 ]
+    await ctx.send(f'{random.choice(responses)}')
+# user join/leave
+
+@client.event
+async def on_member_join(member):
+    print(f'{member} has accepted harakiri.')
+
+@client.event
+async def on_member_remove(member):
+    print(f'{member} has left the world')
+    
+# start msg
 
 @client.event
 async def on_ready():
