55cd4036243caf58680b4163c004e9bcefa733b2
basil <mikus1337112@gmail.com>
2022-06-03 13:32:23 +0300

    message cosmetics

--- cogs/error.py
+++ cogs/error.py
@@ -14,15 +14,15 @@ class Error(commands.Cog):
     @commands.Cog.listener()
     async def on_command_error(self, ctx, error):
         if isinstance(error, commands.MissingRequiredArgument):
-            await ctx.send(embed=discord.Embed(description = 'something is wrong i can feel it in my mind', color=discord.Color.dark_purple()))
+            await ctx.send(embed=discord.Embed(description = 'something is wrong i can feel it in my mind', color=discord.Color.purple()))
         elif isinstance(error, commands.CommandNotFound):
-            await ctx.send(embed=discord.Embed(description = 'i can\'t understand you', color=discord.Color.dark_purple()))
+            await ctx.send(embed=discord.Embed(description = 'i can\'t understand you', color=discord.Color.purple()))
         elif isinstance(error,commands.MissingPermissions):
-            await ctx.send(embed=discord.Embed(description = 'u don\'t have permission >:C', color=discord.Color.dark_purple()))
+            await ctx.send(embed=discord.Embed(description = 'u don\'t have permission >:C', color=discord.Color.purple()))
         elif isinstance(error, commands.CommandRegistrationError):
-            await ctx.send(embed=discord.Embed(description = 'why i can\'t register this? ;(', color=discord.Color.dark_purple()))
+            await ctx.send(embed=discord.Embed(description = 'why i can\'t register this? ;(', color=discord.Color.purple()))
         elif isinstance(error, commands.CommandError):
-            await ctx.send(embed=discord.Embed(description = 'something\'s wrong with this command :(', color=discord.Color.dark_purple()))
+            await ctx.send(embed=discord.Embed(description = 'something\'s wrong with this command :(', color=discord.Color.purple()))
         
 def setup(client):
     client.add_cog(Error(client))
